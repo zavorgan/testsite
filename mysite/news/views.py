5490bff1d40d6df4845fb1f1b8dbbff59c7f9efc
zavorgan <zavorgan@gmail.com>
2020-12-18 01:59:38 +0900

    111

--- mysite/news/views.py
+++ mysite/news/views.py
@@ -1,10 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import News
 
 def index(request):
-    return HttpResponse('Hello world')
+    news = News.objects.all()
+    return render(request, 'news/index.html', {'news': news, 'title': 'Список новостей'})
 
-def test(request):
-    return HttpResponse('<h1>Тестовая страница</h1>')
 
-# Create your views here.
